546c9e082c1f1b3578f345327c002935baa46c4b
Anh Nguyen <a.nguyen0208@gmail.com>
2020-09-30 18:30:59 -0500

    Updates

---
+++ precache-manifest.e3de1103a1621ee31c7b09c307734741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2bf79be3b3cd32a2e33dc38b3259cd1",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "a6b2318d0dbbfe5582da",
+    "url": "/calculator/static/css/main.f5f355ef.chunk.css"
+  },
+  {
+    "revision": "d4b940db2f513e542a47",
+    "url": "/calculator/static/js/2.e3799b46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.e3799b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b2318d0dbbfe5582da",
+    "url": "/calculator/static/js/main.85494d3f.chunk.js"
+  },
+  {
+    "revision": "b7d20be29e90873133b5",
+    "url": "/calculator/static/js/runtime-main.e7880cb4.js"
+  }
+]);
