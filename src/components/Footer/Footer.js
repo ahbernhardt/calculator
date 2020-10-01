43a6e08bd10ba5d9dcfb1ef2795706ae3966d254
Anh Nguyen <a.nguyen0208@gmail.com>
2020-10-01 14:06:42 -0500

    fix link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, {Component} from "react";
 import "./Footer.css";
 
-const Footer = () => (
-    <footer className="footer">
-        <p>
-            Anh Nguyen © 2020
-            <span> <span> </span>| </span>
-            <span>
-                Repo Link: {'  '}
-                <a className="footer-link" href="https://github.com/anguyen0208/calculator" target="_blank" rel="noopener noreferrer">
-                    <i className="fab fa-github" />
-                </a>
-            </span>
-        </p>
-    </footer>
-);
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                <p>
+                    © Copyright 2020 -
+                    <a className="footer-link"
+                       href="https://github.com/anguyen0208"
+                       target="_blank"
+                       rel="noopener noreferrer">
+                        anhnguyen.page
+                    </a>
+                    <span> <span> </span>| </span>
+                    <span>
+                    Repo Link: {'  '}
+                        <a className="footer-link"
+                           href="https://github.com/anguyen0208/calculator"
+                           target="_blank"
+                           rel="noopener noreferrer">
+                        <i className="fab fa-github"/>
+                    </a>
+                </span>
+                </p>
+            </footer>
+        );
+    }
+}
 
 export default Footer;
 
